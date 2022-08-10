6a72891d49086d8472288b741381d593e4b61dfe
Aaishik Dutta <aaidutta@WKMZT6A15623.global.publicisgroupe.net>
2022-08-10 09:38:35 +0530

    added friction

--- main.js
+++ main.js
@@ -102,3 +102,28 @@ AFRAME.registerComponent("autofit-gltf-ammo-sphere", {
   }
 });
 
+AFRAME.registerComponent('friction-coefficient', {
+  schema: {
+    default: 0.8
+  },
+
+  init: function () {
+    // Do something when component first attached.
+    this.el.addEventListener("body-loaded", () => {
+      this.el.body.setFriction(this.data);
+    });
+    
+  },
+
+  update: function () {
+    // Do something when component's data is updated.
+  },
+
+  remove: function () {
+    // Do something the component or its entity is detached.
+  },
+
+  tick: function (time, timeDelta) {
+    // Do something on every scene tick or frame.
+  }
+});
